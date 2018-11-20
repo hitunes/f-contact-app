822a86583b43a41a678d8af890e06c2a943bc7bf
Hitunes247 <itunu.adekoya@tedbree.com>
2018-11-20 11:09:32 +0100

    Change NewContact Class function to Pure function

--- src/components/NewContact/NewContact.jsx
+++ src/components/NewContact/NewContact.jsx
@@ -5,66 +5,64 @@ import NewContactForm from "./NewContactForm";
 import ModalLauncher from "./../Modal/ModalLauncher";
 import * as ContactActions from "../../store/actions/actions";
 
-class NewContact extends React.Component {
+const NewContact = props => {
   // shows New contact modal Onclick
-  showModal = payload => {
-    this.props.showNewContactModal(payload);
+  const showModal = payload => {
+    props.showNewContactModal(payload);
   };
   // hides New Contact modal Onclick
-  hideModal = payload => {
-    this.props.hideNewContactModal(payload);
+  const hideModal = payload => {
+    props.hideNewContactModal(payload);
   };
   // listens to input value change in form and set as new field state
-  handleInputChange = e => {
-    this.props.handleInputChange({ [e.target.name]: e.target.value });
+  const handleInputChange = e => {
+    props.handleInputChange({ [e.target.name]: e.target.value });
   };
   // collects form input value on submit and close modal
-  handleNewContactSubmit = value => {
+  const handleNewContactSubmit = value => {
     document.getElementById("new-contact-form").reset();
     setTimeout(() => {
-      this.props.hideNewContactModal(value);
+      props.hideNewContactModal(value);
     }, 600);
-    this.props.handleNewContactSubmit(value);
+    props.handleNewContactSubmit(value);
   };
-  render() {
-    return (
-      <React.Fragment>
-        <ModalLauncher show={this.props.newContact.showNewContactModal}>
-          <div className="modal-title">
-            <div>Create Contact</div>
-            <div onClick={this.hideModal}>
-              <i className="fas fa-times" />
-            </div>
+  return (
+    <React.Fragment>
+      <ModalLauncher show={props.newContact.showNewContactModal}>
+        <div className="modal-title">
+          <div>Create Contact</div>
+          <div onClick={hideModal}>
+            <i className="fas fa-times" />
           </div>
-          <div className="modal-body">
-            <div className="modal-body--icon-row">
-              <i className="far fa-grin-tongue contact-avatar" />
-              <i className="fas fa-folder-plus" />
-              <i className="fas fa-envelope" />
-              <i className="fas fa-phone" />
-              <i className="fas fa-file-alt" />
-            </div>
-            <NewContactForm
-              handleNewContactSubmit={this.handleNewContactSubmit}
-              handleInputChange={this.handleInputChange}
-            />
+        </div>
+        <div className="modal-body">
+          <div className="modal-body--icon-row">
+            <i className="far fa-grin-tongue contact-avatar" />
+            <i className="fas fa-folder-plus" />
+            <i className="fas fa-envelope" />
+            <i className="fas fa-phone" />
+            <i className="fas fa-file-alt" />
           </div>
-          <div className="modal-footer">
-            <Button onClick={this.hideModal}>Cancel</Button>
-            <Button onClick={this.handleNewContactSubmit}>Save</Button>
-          </div>
-        </ModalLauncher>
-        {this.props.contacts.contactList.length === 0 ? (
-          <span />
-        ) : (
-          <div onClick={this.showModal} className="App-new-contact-icon">
-            <i className="fas fa-plus" />
-          </div>
-        )}
-      </React.Fragment>
-    );
-  }
-}
+          <NewContactForm
+            handleNewContactSubmit={handleNewContactSubmit}
+            handleInputChange={handleInputChange}
+          />
+        </div>
+        <div className="modal-footer">
+          <Button onClick={hideModal}>Cancel</Button>
+          <Button onClick={handleNewContactSubmit}>Save</Button>
+        </div>
+      </ModalLauncher>
+      {props.contacts.contactList.length === 0 ? (
+        <span />
+      ) : (
+        <div onClick={showModal} className="App-new-contact-icon">
+          <i className="fas fa-plus" />
+        </div>
+      )}
+    </React.Fragment>
+  );
+};
 const mapDispatchToProps = dispatch => ({
   showNewContactModal: payload => {
     dispatch(ContactActions.showNewContactModal(payload));
